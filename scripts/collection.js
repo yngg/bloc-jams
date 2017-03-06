0a7cc29343f76c489e23076d143a8451c7526340
Yingnan Zhang <jacksonjustdoit@gmail.com>
2017-03-05 16:52:36 -0800

    checkpoint 30 partial change

--- scripts/collection.js
+++ scripts/collection.js
@@ -1,18 +1,25 @@
-var collectionItemTemplate = 
-      '<div class="collection-album-container column fourth">'
-   + '  <img src="assets/images/album_covers/01.png"/>'
-   + '  <div class="collection-album-info caption">'
-   + '    <p>'
-   + '      <a class="album-name" href="album.html"> The Colors </a>'
-   + '      <br/>'
-   + '      <a href="album.html"> Pablo Picasso </a>'
-   + '      <br/>'
-   + '      X songs'
-   + '      <br/>'
-   + '    </p>'
-   + '  </div>'
-   + '</div>'
-   ;
+// #1
+var collectionItemTemplate = function() {
+    var template = 
+    '<div class="collection-album-container column fourth">'
+    + '  <img src="assets/images/album_covers/01.png"/>'
+    + '  <div class="collection-album-info caption">'
+    + '    <p>'
+    + '      <a class="album-name" href="album.html"> The Colors </a>'
+    + '      <br/>'
+    + '      <a href="album.html"> Pablo Picasso </a>'
+    + '      <br/>'
+    + '      X songs'
+    + '      <br/>'
+    + '    </p>'
+    + '  </div>'
+    + '</div>'
+    ;
+    
+    // #2
+    return $(template);
+    
+}
 
 window.onload = function() {
     //#1
